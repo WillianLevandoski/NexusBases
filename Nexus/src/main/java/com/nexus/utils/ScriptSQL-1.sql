033095dd5f7a7646c4d9e68d72c24403e87b6c80
WillianLevandoski <willian.levandoski@yahoo.com.br>
2020-12-17 22:08:35 -0300

    Ajuste sql

--- Nexus/src/main/java/com/nexus/utils/ScriptSQL-1.sql
+++ Nexus/src/main/java/com/nexus/utils/ScriptSQL-1.sql
@@ -1,27 +1,22 @@
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
+------ Schema mydb
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+create database mydb;
 
+-- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Table `mydb`.`grupo`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`grupo` (
   `id` INT NOT NULL,
-  `nome` VARCHAR(45) NULL,
-  `parametros` VARCHAR(45) NULL,
+  `nome` VARCHAR(45) NULL DEFAULT NULL,
+  `parametros` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -30,18 +25,80 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `mydb`.`usuario` (
   `id` INT NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NULL,
-  `senha` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `senha` VARCHAR(45) NULL DEFAULT NULL,
   `telefone` VARCHAR(11) NOT NULL,
   `grupo_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_table1_grupo_idx` (`grupo_id` ASC) VISIBLE,
-  CONSTRAINT `fk_table1_grupo`
     FOREIGN KEY (`grupo_id`)
-    REFERENCES `mydb`.`grupo` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `mydb`.`grupo` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`cumpomfiscal`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`cumpomfiscal` (
+  `id` INT NOT NULL,
+  `data` VARCHAR(45) NULL DEFAULT NULL,
+  `total` DOUBLE NULL DEFAULT NULL,
+  `forma_pagamento` VARCHAR(45) NULL DEFAULT NULL,
+  `usuario_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `mydb`.`usuario` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`fones`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`fones` (
+  `id` INT NOT NULL,
+  `numero` VARCHAR(11) NULL DEFAULT NULL,
+  `relacao` VARCHAR(45) NULL DEFAULT NULL,
+  `id_relacao` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`fornecedor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`fornecedor` (
+  `id` INT NOT NULL,
+  `nome` VARCHAR(45) NULL DEFAULT NULL,
+  `telefone` VARCHAR(11) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `usuario_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_cliente_usuario1_idx` (`usuario_id` ASC) VISIBLE,
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `mydb`.`usuario` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`produto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`produto` (
+  `id` INT NOT NULL,
+  `descricao` VARCHAR(45) NULL DEFAULT NULL,
+  `codigo_de_barras` VARCHAR(45) NULL DEFAULT NULL,
+  `quantidade` INT NOT NULL,
+  `preco_compra` DOUBLE NULL DEFAULT NULL,
+  `valor_venda` DOUBLE NULL DEFAULT NULL,
+  `margem_lucro` DOUBLE NULL DEFAULT NULL,
+  `fornecedor_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+    FOREIGN KEY (`fornecedor_id`)
+    REFERENCES `mydb`.`fornecedor` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -51,91 +108,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`restricao` (
   `id` INT NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
   `tela` VARCHAR(45) NOT NULL,
-  `consultar` TINYINT NULL,
-  `cadastrar` TINYINT NULL,
-  `alterar` TINYINT NULL,
-  `excluir` TINYINT NULL,
+  `consultar` TINYINT NULL DEFAULT NULL,
+  `cadastrar` TINYINT NULL DEFAULT NULL,
+  `alterar` TINYINT NULL DEFAULT NULL,
+  `excluir` TINYINT NULL DEFAULT NULL,
   `grupo_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_restricao_grupo1_idx` (`grupo_id` ASC) VISIBLE,
-  CONSTRAINT `fk_restricao_grupo1`
     FOREIGN KEY (`grupo_id`)
-    REFERENCES `mydb`.`grupo` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`fornecedor`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`fornecedor` (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(45) NULL,
-  `telefone` VARCHAR(11) NULL,
-  `email` VARCHAR(45) NULL,
-  `usuario_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_cliente_usuario1_idx` (`usuario_id` ASC) VISIBLE,
-  CONSTRAINT `fk_cliente_usuario1`
-    FOREIGN KEY (`usuario_id`)
-    REFERENCES `mydb`.`usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`fones`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`fones` (
-  `id` INT NOT NULL,
-  `numero` VARCHAR(11) NULL,
-  `relacao` VARCHAR(45) NULL,
-  `id_relacao` INT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`produto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`produto` (
-  `id` INT NOT NULL,
-  `descricao` VARCHAR(45) NULL,
-  `codigo_de_barras` VARCHAR(45) NULL,
-  `quantidade` INT NOT NULL,
-  `preco_compra` DOUBLE NULL,
-  `valor_venda` DOUBLE NULL,
-  `margem_lucro` DOUBLE NULL,
-  `fornecedor_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_produto_fornecedor1_idx` (`fornecedor_id` ASC) VISIBLE,
-  CONSTRAINT `fk_produto_fornecedor1`
-    FOREIGN KEY (`fornecedor_id`)
-    REFERENCES `mydb`.`fornecedor` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`cumpomfiscal`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`cumpomfiscal` (
-  `id` INT NOT NULL,
-  `data` VARCHAR(45) NULL,
-  `total` DOUBLE NULL,
-  `forma_pagamento` VARCHAR(45) NULL,
-  `usuario_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_cumpomfiscal_usuario1_idx` (`usuario_id` ASC) VISIBLE,
-  CONSTRAINT `fk_cumpomfiscal_usuario1`
-    FOREIGN KEY (`usuario_id`)
-    REFERENCES `mydb`.`usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `mydb`.`grupo` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -143,28 +125,23 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`venda` (
   `id` VARCHAR(45) NOT NULL,
-  `quantidade` INT NULL,
+  `quantidade` INT NULL DEFAULT NULL,
   `produto_id` INT NOT NULL,
   `cumpomfiscal_id` INT NOT NULL,
-  INDEX `fk_venda_produto1_idx` (`produto_id` ASC) VISIBLE,
   PRIMARY KEY (`id`),
-  INDEX `fk_venda_cumpomfiscal1_idx` (`cumpomfiscal_id` ASC) VISIBLE,
+    FOREIGN KEY (`cumpomfiscal_id`)
+    REFERENCES `mydb`.`cumpomfiscal` (`id`),
   CONSTRAINT `fk_venda_produto1`
     FOREIGN KEY (`produto_id`)
-    REFERENCES `mydb`.`produto` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_venda_cumpomfiscal1`
-    FOREIGN KEY (`cumpomfiscal_id`)
-    REFERENCES `mydb`.`cumpomfiscal` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `mydb`.`produto` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
 
 
 -- -----------------------------------------------------
